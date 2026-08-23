3ebf185e78fd721d916b27b33fe0dc57d84ed6b2
ricardoandaya <ricardoandaya967@gmail.com>
2026-08-23 18:36:30 +0800

    Initialized Window and register Events

---
+++ CarGame/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitilizeGame();
+        }
+
+        private void InitilizeGame()
+        {
+            InitilizeWindow();
+            RegisterEvents();
+        }
+
+        private void InitilizeWindow()
+        {
+            ClientSize = new Size(420, 640);
+            DoubleBuffered = true;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            KeyPreview = true;
+        }
+
+        private void RegisterEvents()
+        {
+            Paint += Form1_Paint;
+            MouseClick += Form1_MouseClick;
+            MouseMove += Form1_MouseMove;
+            KeyUp += Form1_KeyUp;
+            KeyDown += Form1_KeyDown;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+    }
+}
